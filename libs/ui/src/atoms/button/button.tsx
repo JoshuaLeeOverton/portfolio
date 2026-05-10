5fed461715124c932b4c68cf6db765b1b0e6df8e
JoshuaLeeOverton <joshua.leeoverton@gmail.com>
2026-05-10 15:02:59 +0200

    clean up

--- libs/ui/src/atoms/button/button.tsx
+++ libs/ui/src/atoms/button/button.tsx
@@ -1,10 +1,12 @@
+import styles from './button.module.css'
+
 type ButtonProps=  {
   label: string
 }
 
 export function Button({label}: ButtonProps) {
   return (
-    <button>{label}</button>
+    <button className={styles['btn']}>{label}</button>
   );
 }
 
