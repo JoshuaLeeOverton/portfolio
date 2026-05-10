23e4c6434e39b31819a48c9c5d247dfb84290d6c
JoshuaLeeOverton <joshua.leeoverton@gmail.com>
2026-05-10 14:18:16 +0200

    scaffold ui

---
+++ libs/ui/src/lib/ui.tsx
@@ -0,0 +1,11 @@
+import styles from './ui.module.css';
+
+export function PortfolioUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to PortfolioUi!</h1>
+    </div>
+  );
+}
+
+export default PortfolioUi;
